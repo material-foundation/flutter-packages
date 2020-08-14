e1643d92eb4291696099d95b4e996cc059c8571c
Jose Alba <jaguado@google.com>
2020-08-14 10:59:00 +0200

    updated comments

--- packages/adaptive_breakpoints/lib/src/adaptive_container.dart
+++ packages/adaptive_breakpoints/lib/src/adaptive_container.dart
@@ -18,8 +18,6 @@ import 'package:flutter/material.dart';
 class AdaptiveContainer extends StatelessWidget {
   /// Creates a widget that combines common painting, positioning, and sizing widgets.
   ///
-  /// The `height` and `width` values include the padding.
-  ///
   /// The `color` and `decoration` arguments cannot both be supplied, since
   /// it would potentially result in the decoration drawing over the background
   /// color. To supply a decoration with a color, use `decoration:
