11ae7653c3c8451b9e54a543244b50135925116c
Jose Alba <jaguado@google.com>
2020-09-10 12:49:27 +0200

    Adaptive App Bar class

--- packages/adaptive_navigation/example/lib/custom_scaffold.dart
+++ packages/adaptive_navigation/example/lib/custom_scaffold.dart
@@ -14,7 +14,7 @@ class _CustomScaffoldDemoState extends State<CustomScaffoldDemo> {
     return AdaptiveNavigationScaffold(
       selectedIndex: 0,
       destinations: _allDestinations.sublist(0, _destinationCount),
-      appBar: AppBar(title: const Text('Custom Demo')),
+      appBar: AdaptiveAppBar(title: const Text('Custom Demo')),
       body: _body(),
       floatingActionButton: FloatingActionButton(
         child: Icon(Icons.add),
--- packages/adaptive_navigation/lib/scaffold.dart
+++ packages/adaptive_navigation/lib/scaffold.dart
@@ -381,6 +381,66 @@ class AdaptiveNavigationScaffold extends StatelessWidget {
   }
 }
 
+/// AdaptiveAppBar has a leading width of 72.0. Everything else is the same as
+/// [AppBar].
+class AdaptiveAppBar extends AppBar {
+  AdaptiveAppBar({
+    Key key,
+    Widget leading,
+    bool automaticallyImplyLeading = true,
+    Widget title,
+    List<Widget> actions,
+    Widget flexibleSpace,
+    PreferredSizeWidget bottom,
+    double elevation,
+    Color shadowColor,
+    ShapeBorder shape,
+    Color backgroundColor,
+    Brightness brightness,
+    IconThemeData iconThemeData,
+    IconThemeData actionsIconThemeData,
+    TextTheme textTheme,
+    bool primary = true,
+    bool centerTitle,
+    bool excludeHeaderSemantics = false,
+    double titleSpacing = NavigationToolbar.kMiddleSpacing,
+    double toolbarOpacity = 1.0,
+    double bottomOpacity = 1.0,
+    double toolbarHeight,
+    double leadingWidth = 72.0,
+  })  : assert(automaticallyImplyLeading != null),
+        assert(elevation == null || elevation >= 0.0),
+        assert(primary != null),
+        assert(titleSpacing != null),
+        assert(toolbarOpacity != null),
+        assert(bottomOpacity != null),
+        super(
+        key: key,
+        leading: leading,
+        automaticallyImplyLeading: automaticallyImplyLeading,
+        title: title,
+        actions: actions,
+        flexibleSpace: flexibleSpace,
+        bottom: bottom,
+        elevation: elevation,
+        shadowColor: shadowColor,
+        shape: shape,
+        backgroundColor: backgroundColor,
+        brightness: brightness,
+        iconTheme: iconThemeData,
+        actionsIconTheme: actionsIconThemeData,
+        textTheme: textTheme,
+        primary: primary,
+        centerTitle: centerTitle,
+        excludeHeaderSemantics: excludeHeaderSemantics,
+        titleSpacing: titleSpacing,
+        toolbarOpacity: toolbarOpacity,
+        bottomOpacity: bottomOpacity,
+        toolbarHeight: toolbarHeight,
+        leadingWidth: leadingWidth,
+      );
+}
+
 bool _isLargeScreen(BuildContext context) => getWindowType(context) >= AdaptiveWindowType.l;
 
 bool _isMediumScreen(BuildContext context) => getWindowType(context) == AdaptiveWindowType.m;
