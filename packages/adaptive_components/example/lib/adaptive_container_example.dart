2eae8327a5baf1f3725da7a7962d7ab167c416fb
Jose Alba Aguado <jaguado@google.com>
2020-09-22 14:16:39 +0200

    updated examples

--- packages/adaptive_components/example/lib/adaptive_container_example.dart
+++ packages/adaptive_components/example/lib/adaptive_container_example.dart
@@ -17,22 +17,27 @@ class AdaptiveContainerExample extends StatelessWidget {
         body: Column(
           children: [
             AdaptiveContainer(
+              adaptiveConstraints: AdaptiveConstraints.xsmall(),
               color: Colors.red,
               child: Text('This is an extra small window'),
             ),
             AdaptiveContainer(
+              adaptiveConstraints: AdaptiveConstraints.small(),
               color: Colors.orange,
               child: Text('This is a small window'),
             ),
             AdaptiveContainer(
+              adaptiveConstraints: AdaptiveConstraints.medium(),
               color: Colors.yellow,
               child: Text('This is a medium window'),
             ),
             AdaptiveContainer(
+              adaptiveConstraints: AdaptiveConstraints.large(),
               color: Colors.green,
               child: Text('This is a large window'),
             ),
             AdaptiveContainer(
+              adaptiveConstraints: AdaptiveConstraints.xlarge(),
               color: Colors.blue,
               child: Text('This is an extra large window'),
             ),
@@ -40,12 +45,17 @@ class AdaptiveContainerExample extends StatelessWidget {
               adaptiveConstraints: AdaptiveConstraints(
                 xsmall: true,
                 small: true,
+                medium: false,
+                large: false,
+                xlarge: false,
               ),
               color: Colors.indigo,
               child: Text('This is a small or extra small window'),
             ),
             AdaptiveContainer(
               adaptiveConstraints: AdaptiveConstraints(
+                xsmall: false,
+                small: false,
                 medium: true,
                 large: true,
                 xlarge: true,
