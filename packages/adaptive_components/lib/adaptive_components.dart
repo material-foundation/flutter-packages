b9fb2b4f16c30feb12d400d65b7e089b43710347
Levi Lesches <levilesches@gmail.com>
2021-03-23 20:55:17 -0400

    Fixed the adaptive_components library name
    It was `adaptive_breakpoints`, which is confusing beacuse there is another library in this repo with the same name. It also messes up the pub.dev docs.

--- packages/adaptive_components/lib/adaptive_components.dart
+++ packages/adaptive_components/lib/adaptive_components.dart
@@ -2,7 +2,7 @@
 // for details. All rights reserved. Use of this source code is governed by a
 // BSD-style license that can be found in the LICENSE file.
 
-library adaptive_breakpoints;
+library adaptive_components;
 
 export 'src/adaptive_column.dart';
 export 'src/adaptive_container.dart';
