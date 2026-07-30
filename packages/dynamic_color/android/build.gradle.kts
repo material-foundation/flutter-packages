82787af0c37e8c6d42d1a35368452b4998115170
Haroun El Omri <h.elomri@proton.me>
2026-07-30 17:19:30 +0200

    Update to AGP 9 (Java 17, Kotlin 2.3.20)

---
+++ packages/dynamic_color/android/build.gradle.kts
@@ -0,0 +1,57 @@
+group = "io.material.plugins.dynamic_color"
+version = "1.0-SNAPSHOT"
+
+buildscript {
+    val kotlinVersion = "2.3.20"
+    repositories {
+        google()
+        mavenCentral()
+    }
+
+    dependencies {
+        classpath("com.android.tools.build:gradle:8.13.1")
+        classpath("org.jetbrains.kotlin:kotlin-gradle-plugin:$kotlinVersion")
+    }
+}
+
+rootProject.allprojects {
+    repositories {
+        google()
+        mavenCentral()
+    }
+}
+
+plugins {
+    id("com.android.library")
+}
+
+kotlin {
+    compilerOptions {
+        jvmTarget = org.jetbrains.kotlin.gradle.dsl.JvmTarget.JVM_17
+    }
+}
+
+android {
+    namespace = "io.material.plugins.dynamic_color"
+    compileSdk = flutter.compileSdkVersion
+
+    defaultConfig {
+        minSdk = 24
+    }
+
+    compileOptions {
+        sourceCompatibility = JavaVersion.VERSION_17
+        targetCompatibility = JavaVersion.VERSION_17
+    }
+
+    dependencies {
+        val materialVersion = "1.7.0"
+        implementation("com.google.android.material:material:$materialVersion")
+    }
+
+    lint {
+        checkAllWarnings = true
+        warningsAsErrors = true
+        disable.addAll(setOf("AndroidGradlePluginVersion", "InvalidPackage", "GradleDependency", "NewerVersionAvailable"))
+    }
+}
