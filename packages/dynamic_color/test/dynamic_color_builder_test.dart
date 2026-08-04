bcb0bf5cfbc2aba31ff04aa64c0f2ae7791860cc
pierre <6655696+guidezpl@users.noreply.github.com>
2026-08-04 17:50:23 +0200

    add ing suffix

--- packages/dynamic_color/test/dynamic_color_builder_test.dart
+++ packages/dynamic_color/test/dynamic_color_builder_test.dart
@@ -1,5 +1,5 @@
 import 'package:dynamic_color/dynamic_color.dart';
-import 'package:dynamic_color_test/dynamic_color_test.dart';
+import 'package:dynamic_color_testing/dynamic_color_testing.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
 
--- packages/dynamic_color_test/lib/dynamic_color_test.dart
+++
@@ -1,4 +0,0 @@
-library dynamic_color_test;
-
-export 'src/samples.dart';
-export 'src/test_utils.dart';
